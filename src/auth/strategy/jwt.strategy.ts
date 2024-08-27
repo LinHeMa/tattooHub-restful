f950d7b3e8221f487bc1b2ad39a99d14ab6b416a
linhema <linhema0921@gmail.com>
2024-08-27 15:49:32 +0800

    feat(auth): jwt strategy

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -5,17 +5,38 @@ import {
   ExtractJwt,
   Strategy,
 } from 'passport-jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(
   Strategy,
   'jwt',
 ) {
-  constructor(config: ConfigService) {
+  constructor(
+    config: ConfigService,
+    private prisma: PrismaService,
+  ) {
     super({
       jwtFromRequest:
         ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: config.get('JWT_SECRET'),
     });
+  }
+
+  async validate(payload: {
+    sub: number;
+    email: string;
+  }) {
+    const user =
+      await this.prisma.user.findUnique({
+        where: {
+          id: payload.sub,
+        },
+      });
+    /**
+     * remove hash from user object
+     */
+    delete user.hash;
+    return user;
   }
 }
---
+++ src/user/user.controller.ts
@@ -0,0 +1,26 @@
+import {
+  Controller,
+  Get,
+  Patch,
+  UseGuards,
+} from '@nestjs/common';
+import { User } from '@prisma/client';
+import { GetUser } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
+
+@Controller('users')
+export class UserController {
+  @UseGuards(JwtGuard)
+  @Get('me')
+  getMe(
+    @GetUser() user: User,
+    // @GetUser('email') email: string,
+  ) {
+    // console.log({ email });
+    return user;
+  }
+
+  @UseGuards(JwtGuard)
+  @Patch()
+  editUser() {}
+}
